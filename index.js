4061646bbe0bcd62f444171744eeb13e123ae9ea
William Zhao <williamzhao@wireless-10-104-146-187.umd.edu>
2024-12-10 20:05:14 -0500

    Update Files

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 require('dotenv').config();
 const express = require('express')
 const supabaseClient = require('@supabase/supabase-js')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 
 const supabaseUrl = process.env.supabaseUrl;
 const supabaseKey = process.env.supabaseKey;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
 
-app.get('/visitors', (req, res) => {
+app.get('/visitors', async (req, res) => {
     console.log('Attempting to get all vistors.')
-    res.send('Blah')
+
+    const {data, error} = await supabase.from('visitors').select();
+    
+    if (error) {
+        console.log('Error: ', error)
+        res.send(error);
+    } else {
+        console.log('Successfully Retrieved Data');
+        res.send(data);
+    } 
 })
 
-app.post('/visitor', (req, res) => {
+app.post('/visitors', async (req, res) => {
     console.log('Attempting to add vistor.')
+    console.log('Request: ', req.body)
+
+    const ip = req.body.ip;
+    const hostname = req.body.hostname;
+    const continent_code = req.body.continent_code;
+    const continent_name = req.body.continent_name;
+    const country_code = req.body.country_code;
+    const country_name = req.body.country_name;
+    const region_code = req.body.region_code;
+    const region_name = req.body.region_name;
+    const city = req.body.city;
+    const zip = req.body.zip;
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+
+    const {data, error} = await supabase.from('visitors').insert({
+        ip: ip,
+        hostname: hostname,
+        continent_code: continent_code,
+        continent_name: continent_name,
+        country_code: country_code,
+        country_name: country_name,
+        region_code: region_code,
+        region_name: region_name,
+        city: city,
+        zip: zip,
+        latitude: latitude,
+        longitude: longitude
+    })
+    .select();
+
+    if (error) {
+        console.log('Error: ', error)
+        res.send(error);
+    } else {
+        console.log('Successfully Retrieved Data');
+        res.send(data);
+    } 
 })
 
 app.listen(port, () => {
