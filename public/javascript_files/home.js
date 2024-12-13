032316ac5e538e5da5cfae6d1ecfd27783781da1
Kieran Mathis <kmathis9@terpmail.umd.edu>
2024-12-12 20:57:23 -0500

    map should be working now. i cant tell lol

--- public/javascript_files/home.js
+++ public/javascript_files/home.js
@@ -6,31 +6,26 @@ document.getElementById('getUserIP').addEventListener('click', () => {
         .then(data => {
             console.log('User IP Address:', data.ip);
             const userIP = data.ip;
-
+            document.getElementById('userIP').textContent = `User IP: ${userIP}`;
             fetch(`/userself-ip?ip=${userIP}`)
                 .then(response => response.json())
                 .then(data => {
                     const display = ['ip', 'continent_code', 'continent_name', 'country_code',
                         'country_name', 'region_code', 'region_name', 'city', 'zip', 'latitude', 'longitude']
                     document.getElementById('displayBin').innerHTML = `<pre>${JSON.stringify(data, display, 2)}</pre>`;
+                    createMap(data.latitude, data.longitude);
                 })
         })
         .catch(error => console.error('Error Fetching IP Address: ', error));
 })
 
 //Leaflet.js Map Creator
-function createMap() {
-    var map = L.map('map');
+function createMap(latitude, longitude) {
+    var map = L.map('map').setView([latitude, longitude], 15);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: 10,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    map.locate({setView: true})
-    .on('locationerror', function(e){
-        console.log(e);
-        alert("Location access has been denied.");
     });
 }
 
-window.onload = createMap
